37c3137dc1fa0cfd5a681ab97d665d5fc2291531
vidit mathur <viditganpi10@gmail.com>
2022-12-21 13:06:58 +0530

    Login functionality

---
+++ cli/calendar.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+class Calendar():
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/calendar']
+        self.creds = None
+
+    def login(self):
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+    def get_calendar():
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from cli import calendar
+
+def main():
+    calendar_instance = calendar.Calendar()
+    calendar_instance.login()
+
+if __name__ == '__main__':
+    main()
